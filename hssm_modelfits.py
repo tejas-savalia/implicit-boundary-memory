b60c7916c9b51408ea286ad77a3e5e9f3da5f4e6
Tejas Savalia <tejas.savalia7@gmail.com>
2025-08-28 04:34:25 +0000

    Removed unnecessary files

--- hssm_modelfits.py
+++ hssm_modelfits.py
@@ -2,81 +2,100 @@
 # warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import ast, os
 import bambi as bmb
 import pymc as pm
 import arviz as az
-import scipy.stats as stat
-from collections import Counter
-import itertools
 import hssm
-from utils import *
-from hssm.likelihoods import DDM
-import nutpie
+import os, sys
 
-# hssm.set_floatX("float32")
+# 3 CLI arguments
+# 1st: bootstrap or fit
+# 2nd: n_itr (1 for fit)
+# 3rd: structured or unstructured
 
-#reads in all the NAMES of the data files from the 'data' folder. 
-df_clean_exposure = pd.read_csv('df_clean_exposure.csv')
-df_clean_memory = pd.read_csv('df_clean_memory.csv')
+# #reads in all the NAMES of the data files from the 'data' folder. 
+# df_clean_exposure = pd.read_csv('df_clean_exposure.csv')
+# df_clean_memory = pd.read_csv('df_clean_memory.csv')
 
-#Group for each stimulus
-df_clean_exposure_grouped = df_clean_exposure.groupby(['stim chosen', 'participant', 'block']).mean(numeric_only=True).reset_index()
+# #Group for each stimulus
+# df_clean_exposure_grouped = df_clean_exposure.groupby(['stim chosen', 'participant', 'block']).mean(numeric_only=True).reset_index()
 
-#Group for each block (across stimulus, used to assign exposure accuracy to 'new' items for recog test) 
-df_clean_exposure_acc = df_clean_exposure_grouped.groupby(['participant', 'block']).mean(numeric_only=True).reset_index()
+# #Group for each block (across stimulus, used to assign exposure accuracy to 'new' items for recog test) 
+# df_clean_exposure_acc = df_clean_exposure_grouped.groupby(['participant', 'block']).mean(numeric_only=True).reset_index()
 
-#Join first to incorporate exposure accuracy for old stimuli
-df_clean_memory_joined = pd.merge(df_clean_memory, df_clean_exposure_grouped, left_on=['participant', 'test item', 'block'], 
-                                  right_on=['participant', 'stim chosen', 'block'], suffixes=('', '_exposure'), how = 'left')
+# #Join first to incorporate exposure accuracy for old stimuli
+# df_clean_memory_joined = pd.merge(df_clean_memory, df_clean_exposure_grouped, left_on=['participant', 'test item', 'block'], 
+#                                   right_on=['participant', 'stim chosen', 'block'], suffixes=('', '_exposure'), how = 'left')
 
 
-#Replace the new stimuli exposure accuracy NaNs with averages for that participant/block.
-for ppt in df_clean_exposure_acc.participant.unique():
-    for b in range(3):
-        df_clean_memory_joined.loc[((df_clean_memory_joined.node_type == 'new') & (df_clean_memory_joined.participant == ppt) & (df_clean_memory_joined.block == b)), 'accuracy_exposure'] = df_clean_exposure_acc.loc[((df_clean_exposure_acc.participant == ppt)&(df_clean_exposure_acc.block == b)), 'accuracy'].values[0]
+# #Replace the new stimuli exposure accuracy NaNs with averages for that participant/block.
+# for ppt in df_clean_exposure_acc.participant.unique():
+#     for b in range(3):
+#         df_clean_memory_joined.loc[((df_clean_memory_joined.node_type == 'new') & (df_clean_memory_joined.participant == ppt) & (df_clean_memory_joined.block == b)), 'accuracy_exposure'] = df_clean_exposure_acc.loc[((df_clean_exposure_acc.participant == ppt)&(df_clean_exposure_acc.block == b)), 'accuracy'].values[0]
 
+# The operations above are computed and saved in a separate csv file. Just load it instead.
+df_clean_memory_joined = pd.read_csv('df_clean_memory_joined.csv').drop('Unnamed: 0', axis = 1)
 #Remove trials that are too quick (<250 miliseconds)
 df_clean_memory_joined = df_clean_memory_joined.loc[((df_clean_memory_joined.rt > 0.25))].reset_index(drop=True)
 
+itr = int(sys.argv[2])
 
-    
+CONDITION = sys.argv[3]
+
 
 #Parametrize the DDM. The version below is the winning model
+def def_model(boot=True):
+    #If bootstrapping, relabel the node types
+    if boot:
+        for ppt in df_clean_memory_joined.participant.unique():
+            # Randomly select 6 boundary nodes
+            boundary_ids = np.random.choice(np.arange(15), size=6, replace=False)
+            nonboundary_ids = np.delete(np.arange(15), boundary_ids)
 
-ddm_model = hssm.HSSM(noncentered = True, prior_settings = 'safe',  
-                      data = df_clean_memory_joined.loc[df_clean_memory_joined.condition == 'unstructured'], #Specify 'structured' or 'unstructured' condition participants here. Could be cleaner. 
-                      include=[
-                                {
-                                    "name": "v",
-                                    "formula": "v ~ 0 + accuracy_exposure + C(node_type) + C(block) + (C(node_type)|participant_id)",
-                                },
-                          {
-                              "name": "z",
-                              "formula": "z ~ C(block) + (1|participant_id)"
-                          },
+            # reassign labels to randomly selected boundary/nonboundary nodes
+            df_clean_memory_joined.loc[((df_clean_memory_joined['path id'].isin(boundary_ids)) & (df_clean_memory_joined['participant'] == ppt)), 'node_type'] = 'boundary'
+            df_clean_memory_joined.loc[((df_clean_memory_joined['path id'].isin(nonboundary_ids)) & (df_clean_memory_joined['participant'] == ppt)), 'node_type'] = 'nonboundary'
 
-                          {
-                              "name": "a",
-                              "formula": "a ~ C(block) + (1|participant_id)"
+    
+    #Parameterize model.
+    ddm_model = hssm.HSSM(noncentered = True, prior_settings = 'safe',  
+                          data = df_clean_memory_joined.loc[df_clean_memory_joined.condition == CONDITION], #Specify 'structured' or 'unstructured' condition participants here. Could be cleaner. 
+                          include=[
+                                    {
+                                        "name": "v",
+                                        "formula": "v ~ 0 + accuracy_exposure:node_type + node_type + (node_type|participant_id)",
+                                    },
+                              {
+                                  "name": "z",
+                                  "formula": "z ~ C(block) + (1|participant_id)"
+                              },
+    
+                              {
+                                  "name": "a",
+                                  "formula": "a ~ C(block) + (1|participant_id)"
+    
+                              },
+                              
+                              {
+                                  "name": "t",
+                                  "formula": "t ~ (1|participant_id)"
+                              },
+                              
+                              
+                                  ],
+                         )
+    return ddm_model
 
-                          },
-                          
-                          {
-                              "name": "t",
-                              "formula": "t ~ (1|participant_id)"
-                          },
-                          
-                          
-                              ],
-                     )
+#Declare model and Sample
 
-#Sample
-ddm_model_samples = ddm_model.sample(idata_kwargs=dict(log_likelihood=True))
-
-#Save
-az.to_netcdf(ddm_model._inference_obj, f'hssm_results/unstructured_v~acc_exp-block-nodetype-nodetype|ppt_a~block-|ppt_z~block-|ppt_t~|ppt.nc')
-
+if sys.argv[1] == 'bootstrap':
+    ddm_model = def_model()
+    ddm_model.sample()
+    ddm_model.summary().to_csv(f'model_results/bootstrap/{CONDITION}/{itr}.csv')
+    print(f'Bootstrap iteration done: {n_itr+1}')
+else:
+    ddm_model = def_model(boot=False)
+    ddm_model.sample()
+    az.to_netcdf(ddm_model._inference_obj, f'model_results/posteriors/hssm_posteriors/{CONDITION}_v~nt_ntaccexp-|ppt_a~block|ppt_z~block|ppt_t~|ppt.nc')
+    
 
