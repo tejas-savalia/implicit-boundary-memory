eec6564a29bbdea7ef472731f8a69847a8c3c3a1
Tejas Savalia <tejas.savalia7@gmail.com>
2025-09-07 20:38:57 +0000

    Update analyses for revision

--- bootstrap_run.sh
+++ bootstrap_run.sh
@@ -3,9 +3,9 @@
 #SBATCH --mem=16G  # Requested Memory
 #SBATCH -p cpu  # Partition
 #SBATCH -t 2:00:00  # Job time limit
-#SBATCH -o slurm-%j.out  # %j = job ID
-#SBATCH --array=0,97
+#SBATCH -o slurm_logs/slurm-%j.out  # %j = job ID
+#SBATCH --array=26
 
 module load conda/latest
 conda activate eb_music
-python hssm_modelfits.py bootstrap $SLURM_ARRAY_TASK_ID unstructured
+python indiv_fits.py $SLURM_ARRAY_TASK_ID
